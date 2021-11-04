859edf813101522f605d58d6cd78c74ec1b4a328
Imrann John <life.imrann@gmail.com>
2021-11-04 16:29:20 +0100

    Initialisation Projet

---
+++ Main.java
@@ -0,0 +1,28 @@
+package fr.imrann.softwwareregistration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+ Softwareregistration license1 = new Softwareregistration("license1");
+ license1.getLicense();
+ 
+ Softwareregistration license2 = new Softwareregistration("license2");
+ license1.getLicense();
+ 
+ Softwareregistration license3 = new Softwareregistration("license3");
+ license1.getLicense();
+ 
+ Softwareregistration license4 = new Softwareregistration("license4");
+ license1.getLicense();
+ 
+ // Softwareregistration license5 = new Softwareregistration("license5");
+ license1.getLicense();
+
+
+ 
+ 
+	}
+
+}
---
+++ Softwareregistration.java
@@ -0,0 +1,48 @@
+package fr.imrann.softwwareregistration;
+
+public class Softwareregistration {
+
+	private static int nombreDeLicenceMax = 3;
+		
+	public static int getNombreDeLicenceMax() 
+		{
+		return nombreDeLicenceMax;
+		}
+	public static void setNombreDeLicenceMax(int nombreDeLicenceMax) 
+		{
+		Softwareregistration.nombreDeLicenceMax = nombreDeLicenceMax;
+		}
+	
+	private String license;
+	
+	public String setLicense(String license) {
+		this.license = license;
+		return license;
+	}
+	public String getLicense() 
+		{
+		return license;
+		}
+	
+	public Softwareregistration(String license) 
+		{
+	    this.license = license;
+	    
+	    if(getNombreDeLicenceMax()>0) {
+	    	nombreDeLicenceMax --;
+	    System.out.println("Nouvelle license acheté : " + this.license + " - Il vous reste : " + this.nombreDeLicenceMax + " licenses ");
+	    }
+	    else
+	    { 
+	    System.out.println("Nombre de license max atteint - Vous ne pouvez plus creer de nouvelle license !");
+	    }
+		}
+}
+	
+	
+
+	
+    
+	
+	
+
